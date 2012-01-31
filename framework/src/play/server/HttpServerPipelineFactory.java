4bf0faab9c4c590780f3709cda698cbe98d65871
ericsimmerman <eric.simmerman@pascalmetrics.com>
2012-01-30 23:36:40 -0500

    Make support optional

--- framework/src/play/server/HttpServerPipelineFactory.java
+++ framework/src/play/server/HttpServerPipelineFactory.java
@@ -1,32 +1,26 @@
     package play.server;
 
-import static org.jboss.netty.channel.Channels.pipeline;
-
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
-
 import play.Play;
+
+import static org.jboss.netty.channel.Channels.pipeline;
 
 public class HttpServerPipelineFactory implements ChannelPipelineFactory {
 
     public ChannelPipeline getPipeline() throws Exception {
 
         Integer max = Integer.valueOf(Play.configuration.getProperty("play.netty.maxContentLength", "-1"));
-        Boolean bufferChunkedToFiles = Boolean.valueOf(Play.configuration.getProperty("play.netty.filebuffer.chunked.equests"));
+        Boolean supportChunked = Boolean.valueOf(Play.configuration.getProperty("play.netty.supportChunkedRequests"));
            
         ChannelPipeline pipeline = pipeline();
         PlayHandler playHandler = new PlayHandler();
         
         pipeline.addLast("flashPolicy", new FlashPolicyHandler()); 
         pipeline.addLast("decoder", new HttpRequestDecoder());
-        if(bufferChunkedToFiles) {
-            pipeline.addLast("aggregator", new  StreamChunkAggregator(max));
-        }else{
-            pipeline.addLast("aggregator", new  HttpChunkAggregator(max));
-        }
+        pipeline.addLast("aggregator", new StreamChunkAggregator(supportChunked ? max : 1));
         pipeline.addLast("encoder", new HttpResponseEncoder());
         pipeline.addLast("chunkedWriter", playHandler.chunkedWriteHandler);
         pipeline.addLast("handler", playHandler);
