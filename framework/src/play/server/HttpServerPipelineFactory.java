05e70dfd3eeee7db6579d890657067bc38f783eb
ericsimmerman <eric.simmerman@pascalmetrics.com>
2012-02-09 00:34:51 -0500

    Started work on Decoder to pass thru file uploads

--- framework/src/play/server/HttpServerPipelineFactory.java
+++ framework/src/play/server/HttpServerPipelineFactory.java
@@ -1,25 +1,28 @@
     package play.server;
 
+import static org.jboss.netty.channel.Channels.pipeline;
+
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.handler.codec.http.FixedLengthPassthruHttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
-import play.Play;
 
-import static org.jboss.netty.channel.Channels.pipeline;
+import play.Play;
 
 public class HttpServerPipelineFactory implements ChannelPipelineFactory {
 
     public ChannelPipeline getPipeline() throws Exception {
 
-        Integer max = Integer.valueOf(Play.configuration.getProperty("play.netty.maxContentLength", "-1"));        
+        Integer max = Integer.valueOf(Play.configuration.getProperty("play.netty.maxContentLength", "-1"));
+        Boolean passThruFixedLengthRequests = Boolean.valueOf(Play.configuration.getProperty("play.netty.fixedlengthRequests.passthru"));
            
         ChannelPipeline pipeline = pipeline();
         PlayHandler playHandler = new PlayHandler();
         
         pipeline.addLast("flashPolicy", new FlashPolicyHandler()); 
-        pipeline.addLast("decoder", new HttpRequestDecoder());
-        pipeline.addLast("aggregator", new StreamChunkAggregator(max));
+        pipeline.addLast("decoder", passThruFixedLengthRequests ? new FixedLengthPassthruHttpRequestDecoder() : new HttpRequestDecoder());
+        pipeline.addLast("aggregator", new StreamChunkAggregator(max));        
         pipeline.addLast("encoder", new HttpResponseEncoder());
         pipeline.addLast("chunkedWriter", playHandler.chunkedWriteHandler);
         pipeline.addLast("handler", playHandler);
