2224cc833a3d6168ec10c11aaa03fcbf57e43881
ericsimmerman <eric.simmerman@pascalmetrics.com>
2012-02-10 17:22:56 -0500

    Support custom aggregator

--- framework/src/play/server/HttpServerPipelineFactory.java
+++ framework/src/play/server/HttpServerPipelineFactory.java
@@ -4,7 +4,7 @@ import static org.jboss.netty.channel.Channels.pipeline;
 
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.handler.codec.http.FixedLengthPassthruHttpRequestDecoder;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 
@@ -15,14 +15,19 @@ public class HttpServerPipelineFactory implements ChannelPipelineFactory {
     public ChannelPipeline getPipeline() throws Exception {
 
         Integer max = Integer.valueOf(Play.configuration.getProperty("play.netty.maxContentLength", "-1"));
-        Boolean passThruFixedLengthRequests = Boolean.valueOf(Play.configuration.getProperty("play.netty.fixedlengthRequests.passthru"));
+        String customAggregator = Play.configuration.getProperty("play.netty.customAggregator",null);
            
         ChannelPipeline pipeline = pipeline();
         PlayHandler playHandler = new PlayHandler();
         
         pipeline.addLast("flashPolicy", new FlashPolicyHandler()); 
-        pipeline.addLast("decoder", passThruFixedLengthRequests ? new FixedLengthPassthruHttpRequestDecoder() : new HttpRequestDecoder());
-        pipeline.addLast("aggregator", new StreamChunkAggregator(max));        
+        pipeline.addLast("decoder", new HttpRequestDecoder());
+        if(customAggregator != null){
+            SimpleChannelUpstreamHandler aggregator = (SimpleChannelUpstreamHandler)Class.forName(customAggregator).newInstance();
+            pipeline.addLast("aggregator", aggregator);
+        }else{
+            pipeline.addLast("aggregator", new StreamChunkAggregator(max));
+        }
         pipeline.addLast("encoder", new HttpResponseEncoder());
         pipeline.addLast("chunkedWriter", playHandler.chunkedWriteHandler);
         pipeline.addLast("handler", playHandler);
