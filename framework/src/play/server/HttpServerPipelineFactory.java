4d6cb8916cbfcf6d675ed1b63633c66f0f9249f7
ericsimmerman <eric.simmerman@pascalmetrics.com>
2012-11-21 20:45:45 -0500

    Support decompression/compression

--- framework/src/play/server/HttpServerPipelineFactory.java
+++ framework/src/play/server/HttpServerPipelineFactory.java
@@ -3,6 +3,8 @@ package play.server;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpContentCompressor;
+import org.jboss.netty.handler.codec.http.HttpContentDecompressor;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 
@@ -16,10 +18,13 @@ public class HttpServerPipelineFactory implements ChannelPipelineFactory {
 
         Integer max = Integer.valueOf(Play.configuration.getProperty("play.netty.maxContentLength", "-1"));
         Boolean bufferedAggregator = Boolean.parseBoolean(Play.configuration.getProperty("play.netty.bufferChunkedRequests","true"));
+        Boolean requestDecompression = Boolean.parseBoolean(Play.configuration.getProperty("play.netty.requestDecompression","true"));
+        Boolean responseCompression = Boolean.parseBoolean(Play.configuration.getProperty("play.netty.responseCompression","false"));
            
         ChannelPipeline pipeline = pipeline();
         PlayHandler playHandler = new PlayHandler();
         
+        if(requestDecompression) pipeline.addLast("decompressor", new HttpContentDecompressor());
         pipeline.addLast("flashPolicy", new FlashPolicyHandler()); 
         pipeline.addLast("decoder", new HttpRequestDecoder());        
         pipeline.addLast("aggregator", bufferedAggregator ? new StreamChunkAggregator(max) : new StreamingChunkAggregator());
@@ -27,6 +32,7 @@ public class HttpServerPipelineFactory implements ChannelPipelineFactory {
         pipeline.addLast("encoder", new HttpResponseEncoder());
         pipeline.addLast("chunkedWriter", playHandler.chunkedWriteHandler);
         pipeline.addLast("handler", playHandler);
+        if(responseCompression) pipeline.addLast("compressor", new HttpContentCompressor());
 
         return pipeline;
     }
